067a0d8671fd20905348fbefa3a73d0c08f1a9c1
Sai2521 <saipadhy44@gmail.com>
2026-01-17 05:27:14 +0530

    Refactor router_reg_tb to router_sync_tb

--- SYNCHRONIZER/router_sync_tb.v
+++ SYNCHRONIZER/router_sync_tb.v
@@ -1,138 +1,95 @@
-module router_reg_tb();
+module router_sync_tb();
+	wire [2:0]write_enb;
+	wire fifo_full;
+	wire vld_out_0,vld_out_1,vld_out_2;
+	wire soft_reset_0,soft_reset_1,soft_reset_2;
+	reg  clock,resetn,detect_add;
+	reg  [1:0]data_in;
+	reg  full_0,full_1,full_2;
+	reg  empty_0,empty_1,empty_2;
+        reg write_enb_reg;
+        reg read_enb_0,read_enb_1,read_enb_2;
+        parameter CYCLE=10;
+    
+router_sync DUT(clock,resetn,data_in,detect_add,full_0,full_1,full_2,empty_0,empty_1,empty_2,write_enb_reg,read_enb_0,read_enb_1,read_enb_2,write_enb,fifo_full,vld_out_0,vld_out_1,vld_out_2,soft_reset_0,soft_reset_1,soft_reset_2);
+	
+          initial 
+            begin
+              clock=1'b0;
+              forever #(CYCLE/2) clock=~clock;
+            end      
+//task initialize	
+           task initialize;
+ 	        begin
+		     {detect_add,data_in,full_0,full_1,full_2}=0;
+		     {write_enb_reg,read_enb_0,read_enb_1,read_enb_2,empty_0,empty_1,empty_2}=0;
 
-reg clock,resetn,pkt_valid,fifo_full,detect_add,ld_state,laf_state,full_state,lfd_state,rst_int_reg;
-reg [7:0]data_in;
-wire err,parity_done,low_packet_valid;
-wire [7:0]dout;
-integer i;
-parameter cycle=10;
+	        end
+	       endtask
+//task reset
+	      task reset_dut();
+                  begin
+                    @(negedge clock)
+                      resetn=1'b0; 
+		            @(negedge clock)
+                      resetn=1'b1; 
+                  end
+              endtask
+//task readenb
+                task readenb(input r1,r2,r3);
+                   begin
+                        {read_enb_0,read_enb_1,read_enb_2}={r1,r2,r3};
+		            end 
+               endtask
+//task input and detect addresee
+                 task detect_ad(input [1:0]d1,input detect_ad1);
+                    begin
+                      data_in=d1;
+		              detect_add=detect_ad1;
+                    end
+      		endtask
+//task fifo_full     
+		task fifo_ful(input f1,f2,f3);
+                    begin
+                      full_0=f1;
+                      full_1=f2;
+                      full_2=f3;
+                     end
+                endtask
+//task empty
+               task empty_dut(input e1,e2,e3);
+                  begin
+		            empty_0=e1;
+		            empty_1=e2;
+                    empty_2=e3;
+		  		  end
+			    endtask 
+  
+  //task write enb reg
+                task write_reg(input l1);
+                    begin
+                    write_enb_reg=l1;
 
-
-router_reg DUT(clock,resetn,pkt_valid,data_in,fifo_full,detect_add,ld_state,laf_state,full_state,lfd_state,rst_int_reg,err,parity_done,low_packet_valid,dout);
+                    end
+               endtask
+       initial
+         begin
+		initialize;
+		reset_dut();
+                @(negedge clock)
+		readenb(1,1,0);
+        detect_ad(2'b10,1);
+           fifo_ful(0,0,0);
+        write_reg(1);
+           empty_dut(0,0,0);
+                #1000;
+           $finish;
+         end
 
 initial
-begin
-  clock=1'b0;
-  forever #(cycle/2) clock=~clock;
-end
-
-task rst();
-  begin
-    @(negedge clock)
-    resetn=1'b0;
-    @(negedge clock)
-    resetn=1'b1;
-  end
-endtask
-
-task initialize();
-  begin
-   pkt_valid<=1'b0;
-   fifo_full<=1'b0;
-   detect_add<=1'b0;
-   ld_state<=1'b0;
-   laf_state<=1'b0;
-   full_state<=1'b0;
-   lfd_state<=1'b0;
-   rst_int_reg<=1'b0;
-  end
-endtask
-
-task good_pkt_gen_reg;
-
-reg[7:0]payload_data,parity1,header1;
-reg[5:0]payload_len;
-reg[1:0]addr;
-
-begin
- @(negedge clock)
- payload_len=6'd5;
- addr=2'b10;
- pkt_valid=1;
- detect_add=1;
- header1={payload_len,addr};
- parity1=0^header1;
- data_in=header1;
- @(negedge clock);
- detect_add=0;
- lfd_state=1;
- full_state=0;
- fifo_full=0;
- laf_state=0;
- for(i=0;i<payload_len;i=i+1)
- begin
- @(negedge clock);
-  lfd_state=0;
-  ld_state=1;
-  payload_data={$random}%256;
-  data_in=payload_data;
-  parity1=parity1^data_in;
- end
- @(negedge clock);
- pkt_valid=0;
- data_in=parity1;
- @(negedge clock);
- ld_state=0;
- end
- endtask
-
-
-task bad_pkt_gen_reg;
-
-reg[7:0]payload_data,parity1,header1;
-reg[5:0]payload_len;
-reg[1:0]addr;
-
-begin
- @(negedge clock)
- payload_len=6'd5;
- addr=2'b10;
- pkt_valid=1;
- detect_add=1;
- header1={payload_len,addr};
- parity1=0^header1;
- data_in=header1;
- @(negedge clock);
- detect_add=0;
- lfd_state=1;
- full_state=0;
- fifo_full=0;
- laf_state=0;
- for(i=0;i<payload_len;i=i+1)
- begin
- @(negedge clock);
-  lfd_state=0;
-  ld_state=1;
-  payload_data={$random}%256;
-  data_in=payload_data;
-  parity1=parity1^data_in;
- end
- @(negedge clock);
- pkt_valid=0;
- data_in=46;
- @(negedge clock);
- ld_state=0;
- end
- endtask
- 
- 
- initial
- begin
- rst();
- initialize();
- good_pkt_gen_reg;
- rst();
- bad_pkt_gen_reg;
- #20
- 
- $finish;
- end
-
- 
- initial
- begin
- $dumpfile("Router_reg.vcd");
- $dumpvars;
- end
-
- endmodule
+     begin
+       $dumpfile("router_sync_tb.vcd");
+	$dumpvars();
+	
+     end
+endmodule
